e8dc8d9144b32dff2d3d696b94de417f74adc11b
govinshinde <govinshinde@gmail.com>
2023-05-27 15:29:18 +0530

    Update merge-bulk-contact.component.ts

--- src/app/merge-bulk-contact/merge-bulk-contact.component.ts
+++ src/app/merge-bulk-contact/merge-bulk-contact.component.ts
@@ -100,7 +100,7 @@ export class MergeBulkContactComponent {
   mergeContact() {
     if (this.idArr.length > 0) {
       this.spinner.show();
-      this.httpClient.post<string>(`http://localhost:5005/archiveindvidual/${this.idArr.toString()}`, this.mapArr).subscribe({
+      this.httpClient.post<string>(`https://afsp-java-virtuous-app.herokuapp.com/archiveindvidual/${this.idArr.toString()}`, this.mapArr).subscribe({
         next: data => {
           this.status = data;
         },
